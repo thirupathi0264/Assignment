654e95d17907141df874ed88262eb632cffa06ef
Thirupathi Reddy <thirupathi.r@prodapt.com>
2022-07-19 09:50:22 +0530

    source code

---
+++ SchoolRecords.java
@@ -0,0 +1,13 @@
+package com.prodapt.OOPbasics;
+
+public class SchoolRecords {
+	int schoolId;
+	String schoolName;
+	String schoolAddress;
+	void addRecords(int schoolId, String schoolName, String schoolAddress) {
+	System.out.println(schoolId + " " + " " + schoolName + " " + schoolAddress);
+	}
+	void displayRecords() {
+	System.out.println("Records of Schools in Telangana");
+}
+}
