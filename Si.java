dc40b7feea917f68a5ac6e6cf304d2163b24a971
Thirupathi Reddy <thirupathi.r@prodapt.com>
2022-07-19 09:53:21 +0530

    souce code

---
+++ Si.java
@@ -0,0 +1,15 @@
+package com.prodapt.OOPbasics;
+
+public class Si {
+	static int simple_Interest;
+	static int principle;
+	static int rate;
+	 static int time;
+	static int total_Amount;
+	static void simpleInterest() {
+	simple_Interest = ((principle*rate*time)/100);
+	System.out.println("SimpleInterest = " + simple_Interest);
+	total_Amount = principle + simple_Interest;
+	System.out.println("Amount to be paid after Loan = " + total_Amount);
+}
+}
